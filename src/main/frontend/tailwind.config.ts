75b8853ce310282498308a766cbf118922fdaa71
Allen <allen.gueco1@gmail.com>
2024-03-02 19:24:59 -0500

    tailwind setup

---
+++ src/main/frontend/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type {Config} from 'tailwindcss'
+
+export default {
+    content: ["../resources/templates/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+} satisfies Config
+
