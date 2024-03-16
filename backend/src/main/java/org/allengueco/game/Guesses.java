ca9048005b68fc87f586c50365037aa8b7ed9eb7
Allen <allen.gueco1@gmail.com>
2024-03-16 04:42:24 -0400

    guesses serde

--- backend/src/main/java/org/allengueco/game/Guesses.java
+++ backend/src/main/java/org/allengueco/game/Guesses.java
@@ -3,24 +3,22 @@ package org.allengueco.game;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.deser.std.MapDeserializer;
-import com.fasterxml.jackson.databind.ser.Serializers;
-import com.fasterxml.jackson.databind.ser.std.MapSerializer;
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 import org.eclipse.collections.impl.set.sorted.mutable.TreeSortedSet;
-import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.data.annotation.Transient;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Set;
-@JsonSerialize(keyAs = String.class, contentAs = List.class)
-@JsonDeserialize(keyAs = String.class, contentAs = List.class)
+
 public class Guesses {
     @JsonIgnore
+    @Transient
     public final Comparator<String> comparator = String.CASE_INSENSITIVE_ORDER;
-
+    @JsonSerialize(as = Set.class)
+    @JsonDeserialize(as = Set.class)
     private final Set<String> before = TreeSortedSet.newSet(comparator);
+    @JsonSerialize(as = Set.class)
+    @JsonDeserialize(as = Set.class)
     private final Set<String> after = TreeSortedSet.newSet(comparator);
 
     public Guesses() {
