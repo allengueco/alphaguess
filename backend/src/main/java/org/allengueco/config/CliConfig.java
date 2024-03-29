303879bb45344ef03662d7095f5213543d575c37
Allen Gueco <allen.gueco1@gmail.com>
2024-03-28 22:21:47 -0400

    more cli wip

--- backend/src/main/java/org/allengueco/config/CliConfig.java
+++ backend/src/main/java/org/allengueco/config/CliConfig.java
@@ -1,13 +1,69 @@
 package org.allengueco.config;
 
 import org.allengueco.commands.Submit;
+import org.allengueco.service.BetaGuessClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.shell.command.annotation.EnableCommand;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
 
 @EnableCommand(Submit.class)
 @ConditionalOnProperty(value = "cli")
 @Configuration(proxyBeanMethods = false)
 public class CliConfig {
+    private static final Logger log = LoggerFactory.getLogger(CliConfig.class);
 
+    @Bean
+    BetaGuessClient betaGuessClient(RestClient restClient) {
+        return HttpServiceProxyFactory
+                .builderFor(RestClientAdapter.create(restClient))
+                .build()
+                .createClient(BetaGuessClient.class);
+    }
+
+    @Bean
+    RestClient restClient(ClientHttpRequestInterceptor interceptor) {
+        return RestClient.builder()
+                .baseUrl("http://localhost:8080")
+                .requestFactory(new JdkClientHttpRequestFactory(HttpClient.newBuilder().build()))
+                .requestInterceptor(interceptor)
+                .build();
+    }
+
+    @Bean
+    ClientHttpRequestInterceptor interceptor() {
+        return new StatefulInterceptor();
+    }
+
+    private static class StatefulInterceptor implements ClientHttpRequestInterceptor {
+        private String cookie;
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+            if (this.cookie != null) {
+                request.getHeaders().add(HttpHeaders.COOKIE, this.cookie);
+            }
+            var response = execution.execute(request, body);
+
+            if (this.cookie == null) {
+                cookie = response.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
+            }
+
+            return response;
+        }
+    }
 }
---
+++ backend/src/main/java/org/allengueco/service/BetaGuessClient.java
@@ -0,0 +1,16 @@
+package org.allengueco.service;
+
+import org.allengueco.dto.SubmitRequest;
+import org.allengueco.game.GameSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@HttpExchange(url = "/api", accept = APPLICATION_JSON_VALUE, contentType = APPLICATION_JSON_VALUE)
+public interface BetaGuessClient {
+    @PostExchange("/submit")
+    ResponseEntity<GameSession> submitGuess(@RequestBody SubmitRequest request);
+}
