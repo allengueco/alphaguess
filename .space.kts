09c7eba40f6f3306c9e9760d7661bf2b2c8cf136
Allen <allen.gueco1@gmail.com>
2024-04-21 02:25:02 -0400

    space kts

--- .space.kts
+++ .space.kts
@@ -1,7 +1,7 @@
 job("Build and test") {
     container(image = "eclipse-temurin:21") {
-        env["BETAGUESS_CLIENT_ID"] = "{{ project:betaguess_client }}"
-        env["BETAGUESS_CLIENT_SECRET"] = "{{ project:betaguess_client_secret }}"
+        env["BETAGUESS_CLIENT_ID"] = "{{ project:BETAGUESS_CLIENT_ID }}"
+        env["BETAGUESS_CLIENT_SECRET"] = "{{ project:BETAGUESS_CLIENT_SECRET }}"
         kotlinScript { api ->
             api.gradlew("bootBuildImage")
         }
