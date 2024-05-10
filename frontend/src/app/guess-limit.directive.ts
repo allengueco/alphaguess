306dbad1cb5a73c9f8da6eaea7c3c34d2eb1b2a7
Allen Gueco <allen.gueco1@gmail.com>
2024-05-10 14:58:15 -0400

    lifecycle

--- frontend/src/app/guess-limit.directive.ts
+++ frontend/src/app/guess-limit.directive.ts
@@ -1,19 +1,18 @@
-import { AfterContentInit, AfterViewChecked, AfterViewInit, contentChildren, Directive, ElementRef, inject, input, Renderer2, viewChildren } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, contentChildren, Directive, ElementRef, inject, input, Renderer2, viewChildren } from '@angular/core';
 
 @Directive({
   selector: '[appGuessLimit]',
   standalone: true
 })
-export class GuessLimitDirective implements AfterContentInit {
+export class GuessLimitDirective implements AfterViewInit {
   limit = input(5)
   renderer = inject(Renderer2)
   position = input.required<'first'|'last'>()
 
-  guesses = contentChildren<ElementRef>('h2')
+  guesses = viewChildren<ElementRef>('h2')
 
-  ngAfterContentInit(): void {
+  ngAfterViewInit(): void {
     this.guesses().forEach((c, i, _) => {
-      console.log(c)
       this.renderer.setStyle(c, 'display', 'none')
     })
   }
